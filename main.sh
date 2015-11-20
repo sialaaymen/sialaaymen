616a5e821560b088b1edbe319409c9b7fdba48f5
sialaaymen <sialaaymengmail.com>
2015-11-20 14:49:59 +0000

    more serious

--- main.sh
+++ main.sh
@@ -2,5 +2,6 @@
 git add main.sh
 git config user.name "sialaaymen"
 git commit -m "more serious"
-git remote add aymanov http://github.com/sialaaymen/sialaaymen
+#git remote add aymanov http://github.com/sialaaymen/sialaaymen
+git remote
 git git push -u aymanov master
